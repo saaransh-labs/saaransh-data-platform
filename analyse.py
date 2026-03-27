539ace0e4c5e6764c7e070cdc00686a9a97c9274
Sudip Chowdhury <sudip4@live.com>
2026-03-27 08:53:39 +0530

    chore: reset repo for fresh start

--- analyse.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("transcripts.csv")
-counts = df["Page"].value_counts()
-low_frequency_pages = counts[counts < 10].index.tolist()
-
-print(low_frequency_pages)
